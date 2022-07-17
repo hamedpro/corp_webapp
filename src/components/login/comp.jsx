ba01ae513783ff81bb9ea002179f6cec587d4c70
hamedpro <hamedyaghootpour@gmail.com>
2022-07-17 20:27:40 +0430

    backup

--- src/components/login/comp.jsx
+++ src/components/login/comp.jsx
@@ -1,8 +1,10 @@
 import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { customAjax } from "../../../common-codes/custom_api_system/dev/custom_ajax.js";
+//import global_context from "../../global_context";
 export default function Login() {
   var navigate = useNavigate();
+  //var context = useContext(global_context);
   function login(username, password) {
     customAjax({
       params: {
@@ -15,6 +17,7 @@ export default function Login() {
       (data) => {
         if (data.result) {
           alert("auth was performed");
+          // context.set_context_data({ username }); // todo take care to not override existing data in context data state in app.jsx
           navigate("/");
         } else {
           alert("username or password was incorrect please try again");
--- src/components/new_support_ticket/comp.jsx
+++ src/components/new_support_ticket/comp.jsx
@@ -1,5 +1,37 @@
-export default function NewSupportTicket(){
-  return(
-    <p>new support ticket page</p>
-  )
+import { useContext, useEffect } from "react";
+import { customAjax } from "../../../common-codes/custom_api_system/dev/custom_ajax";
+
+export default function NewSupportTicket() {
+  function new_supprot_ticket() {
+    customAjax({
+      params: {
+        task_name: "new_support_ticket",
+        username: "",
+        text: document.getElementById("support_ticket_text_input"),
+      },
+    }).then(
+      (data) => {
+        if (data.result) {
+          alert("done");
+        } else {
+          alert("result field was not true");
+        }
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+  return (
+    <>
+      <h1>new support ticket</h1>
+      <hr />
+      <p>enter support ticket text:</p>
+      <textarea
+        id="support_ticket_text_input"
+        className="border border-green-600"
+      ></textarea>
+      <button onClick={new_supprot_ticket}>submit support ticket</button>
+    </>
+  );
 }
