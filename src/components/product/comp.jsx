ce84e208abacf20fa7ce39f9df059e1d43c849fb
hamedpro <hamedyaghootpour@gmail.com>
2022-07-29 05:00:40 +0430

    loading images and image panel in product page are working now

--- src/components/product/comp.jsx
+++ src/components/product/comp.jsx
@@ -2,89 +2,131 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { customAjax } from "../../../common-codes/custom_api_system/dev/custom_ajax.js";
 export default function Product() {
-  var product_id = useParams().product_id;
-  const [product, set_product] = useState({
-    id: "loading ...",
-    name: "loading ...",
-    description: "loading ...",
-    price: "loading ...",
-    product_specs: "[]",
-  });
+	var product_id = useParams().product_id;
+	const [product, set_product] = useState({
+		id: "loading ...",
+		name: "loading ...",
+		description: "loading ...",
+		price: "loading ...",
+		product_specs: "[]",
+	});
 
-  function upload_files() {
-    var files = document.getElementById("files_input").files;
-    var form = new FormData();
-    for (var i = 0; i < files.length; i++) {
-      form.append(i, files[i]);
-    }
-    fetch(
-      "http://localhost:4000?task_name=upload_product_images&product_id=" +
-        product_id,
-      {
-        method: "POST",
-        body: form,
-      }
-    );
-  }
-  function fetch_data() {
-    customAjax({
-      params: {
-        task_name: "get_products",
-      },
-    }).then(
-      (data) => {
-        set_product(data.result.filter((i) => i.id == Number(product_id))[0]);
-      },
-      (error) => {
-        console.log(error);
-      }
-    );
-  }
-  useEffect(() => {
-    fetch_data();
-  }, []);
-  return (
-    <>
-      <div className="mx-auto border border-blue-400 rounded mt-2 p-2">
-        <h1>product page</h1>
-        <hr />
-        <p>product id : {product.id}</p>
-        <p>product name : {product.name}</p>
-        <p>product description : {product.description}</p>
-        <p>product price : {product.price}</p>
-      </div>
-      <div className="mx-auto border border-blue-400 rounded mt-2 p-2">
-        <h1 className="text-lg">product specs:</h1>
-        <hr className="mb-2" />
+	function upload_files() {
+		var files = document.getElementById("files_input").files;
+		var form = new FormData();
+		for (var i = 0; i < files.length; i++) {
+			form.append(i, files[i]);
+		}
+		fetch("http://localhost:4000?task_name=upload_product_images&product_id=" + product_id, {
+			method: "POST",
+			body: form,
+		});
+	}
+	function fetch_data() {
+		customAjax({
+			params: {
+				task_name: "get_products",
+			},
+		}).then(
+			(data) => {
+				set_product(data.result.filter((i) => i.id == Number(product_id))[0]);
+			},
+			(error) => {
+				console.log(error);
+			}
+		);
+	}
+	useEffect(() => {
+		fetch_data();
+	}, []);
+	var [image_sources, set_image_sources] = useState([]);
+	useEffect(() => {
+		customAjax({
+			params: {
+				task_name: "get_paths_of_images_of_a_product",
+				product_id,
+			},
+		}).then((data) => {
+			set_image_sources(
+				data.result.map((file_name) => "http://localhost:4000/product_images/" + file_name)
+			);
+		});
+	}, []);
+	var [current_image_index, set_current_image_index] = useState(0);
+	function image_next() {
+		if (current_image_index + 1 <= image_sources.length - 1) {
+			set_current_image_index(current_image_index + 1);
+		} else {
+			set_current_image_index(0);
+		}
+	}
+	function image_back() {
+		if (current_image_index - 1 >= 0) {
+			set_current_image_index(current_image_index - 1);
+		} else {
+			set_current_image_index(image_sources.length - 1);
+		}
+	}
+	return (
+		<>
+			<div className="mx-auto border border-blue-400 rounded mt-2 p-2">
+				<div className="flex justify-center align-center relative mx-auto mt-0 mb-1 rounded bg-blue-100 h-48 w-full">
+					<button
+						onClick={() => {
+							image_back();
+						}}
+					>
+						back
+					</button>
+					{image_sources.length == 0 ? (
+						<>there is not any product image uploaded for this product</>
+					) : (
+						<img
+							className="h-full"
+							src={image_sources[current_image_index]}
+							alt="product image"
+						/>
+					)}
+					<button
+						onClick={() => {
+							image_next();
+						}}
+					>
+						next
+					</button>
+				</div>
+				<h1>product page</h1>
+				<hr />
+				<p>product id : {product.id}</p>
+				<p>product name : {product.name}</p>
+				<p>product description : {product.description}</p>
+				<p>product price : {product.price}</p>
+			</div>
+			<div className="mx-auto border border-blue-400 rounded mt-2 p-2">
+				<h1 className="text-lg">product specs:</h1>
+				<hr className="mb-2" />
 
-        <table className="custom_border">
-          <tbody>
-            <tr>
-              <th>key</th>
-              <th>value</th>
-            </tr>
-            {JSON.parse(product.product_specs).map((spec) => {
-              return (
-                <tr key={spec.id}>
-                  <td>{spec.key}</td>
-                  <td>{spec.value}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
-      <div className="mx-auto border border-blue-400 rounded mt-2 p-2">
-        <h1 className="text-lg">product specs:</h1>
-        <hr className="mb-2" />
-        <input type="file" id="files_input" multiple />
-        <button
-          onClick={upload_files}
-          className="border border-blue-400 rounded"
-        >
-          upload files
-        </button>
-      </div>
-    </>
-  );
+				<table className="custom_border">
+					<tbody>
+						<tr>
+							<th>key</th>
+							<th>value</th>
+						</tr>
+						{JSON.parse(product.product_specs).map((spec) => {
+							return (
+								<tr key={spec.id}>
+									<td>{spec.key}</td>
+									<td>{spec.value}</td>
+								</tr>
+							);
+						})}
+					</tbody>
+				</table>
+			</div>
+			<div className="mx-auto border border-blue-400 rounded mt-2 p-2">
+				<h1 className="text-lg">product specs:</h1>
+				<hr className="mb-2" />
+			</div>
+		</>
+	);
 }
--- src/components/products/comp.jsx
+++ src/components/products/comp.jsx
@@ -2,78 +2,73 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { customAjax } from "../../../common-codes/custom_api_system/dev/custom_ajax.js";
 export default function Products() {
-  var nav = useNavigate();
-  function ProductItem({ id, name, price }) {
-    var [the_image_src, set_the_image_src] = useState(null);
-    useEffect(() => {
-      customAjax({
-        params: {
-          task_name: "get_paths_of_images_of_a_product",
-          product_id: id,
-        },
-      }).then((data) => {
-        if (data.result.length != 0) {
-          set_the_image_src(
-            "http://localhost:4000/product_images/" + data["result"][0]
-          );
-        } else {
-          set_the_image_src(null);
-        }
-      });
-    }, []);
-
-    return (
-      <div
-        onClick={() => nav("/product/" + id)}
-        className="cursor-pointer my-2 w-5/12 block border border-1 border-blue-400 rounded mx-auto relative p-1"
-      >
-        <div className="w-11/12 h-28 mx-auto my-2 bg-blue-500 rounded flex items-center justify-center">
-          {the_image_src === null ? (
-            <h1 className="text-center">
-              there is not any image for this product
-            </h1>
-          ) : (
-            <img src={the_image_src} alt="this product's first image" />
-          )}
-        </div>
-        <p className="p-1">
-          #{id} : {name}
-        </p>
-        <b>price : {price} toman</b>
-      </div>
-    );
-  }
-  const [products, set_products] = useState([]);
-  useEffect(() => {
-    customAjax({
-      params: {
-        task_name: "get_products",
-      },
-    }).then(
-      (data) => {
-        set_products(data.result);
-      },
-      (error) => {
-        console.log(error);
-      }
-    );
-  }, []);
-  return (
-    <div className="mx-auto border border-blue-400 rounded mt-2 p-2 px-0">
-      <h1 className="text-lg">products page</h1>
-      <hr />
-      <div className="flex flex-wrap ">
-        {products.map((product) => {
-          return (
-            <ProductItem
-              id={product.id}
-              name={product.name}
-              price={product.price}
-              key={product.id}
-            />
-          );
-        })}
-      </div>
-    </div>
-  );
+	var nav = useNavigate();
+	function ProductItem({ id, name, price }) {
+		var [the_image_src, set_the_image_src] = useState(null);
+		useEffect(() => {
+			customAjax({
+				params: {
+					task_name: "get_paths_of_images_of_a_product",
+					product_id: id,
+				},
+			}).then((data) => {
+				if (data.result.length != 0) {
+					set_the_image_src("http://localhost:4000/product_images/" + data["result"][0]);
+				} else {
+					set_the_image_src(null);
+				}
+			});
+		}, []);
+		return (
+			<div
+				onClick={() => nav("/product/" + id)}
+				className="cursor-pointer my-2 w-5/12 block border border-1 border-blue-400 rounded mx-auto relative p-1"
+			>
+				<div className="w-11/12 h-28 mx-auto my-2 bg-blue-500 rounded flex items-center justify-center">
+					{the_image_src === null ? (
+						<h1 className="text-center">there is not any image for this product</h1>
+					) : (
+						<img src={the_image_src} alt="this product's first image" />
+					)}
+				</div>
+				<p className="p-1">
+					#{id} : {name}
+				</p>
+				<b>price : {price} toman</b>
+			</div>
+		);
+	}
+	const [products, set_products] = useState([]);
+	useEffect(() => {
+		customAjax({
+			params: {
+				task_name: "get_products",
+			},
+		}).then(
+			(data) => {
+				set_products(data.result);
+			},
+			(error) => {
+				console.log(error);
+			}
+		);
+	}, []);
+	return (
+		<div className="mx-auto border border-blue-400 rounded mt-2 p-2 px-0">
+			<h1 className="text-lg">products page</h1>
+			<hr />
+			<div className="flex flex-wrap ">
+				{products.map((product) => {
+					return (
+						<ProductItem
+							id={product.id}
+							name={product.name}
+							price={product.price}
+							key={product.id}
+						/>
+					);
+				})}
+			</div>
+		</div>
+	);
 }
