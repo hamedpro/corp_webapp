e3657e43b7696071807dc720dca987a67b14e37a
hamedpro <hamedyaghootpour@gmail.com>
2022-09-19 03:31:28 +0430

    "/blog-posts/:blog_id" now loads data from api and shows them graphically

--- src/components/blog/comp.jsx
+++ src/components/blog/comp.jsx
@@ -1,33 +1,38 @@
-import { InfoRounded } from "@mui/icons-material";
 import Section from "../section/comp";
-import { Alert } from "../alert/comp";
 import { useParams } from "react-router-dom";
-import { multi_lang_helper as ml } from "../../common";
+import { gen_link_to_file, multi_lang_helper as ml } from "../../common";
+import { useEffect, useState } from "react";
+import { customAjax } from "../../custom_ajax";
+import { Loading } from "../loading/comp";
 export default function Blog() {
 	var blog_id = useParams().blog_id;
+	var [blog, set_blog] = useState(null)
+	function fetch_data() {
+		customAjax({
+			params: { 
+				task_name : "get_blog_posts"
+			}
+		}).then(data => {
+			set_blog(data.result.find(i=> i.id == blog_id))
+		}, e => {
+			console.log(e)
+		})
+	}
+	useEffect(fetch_data,[])
 	return (
 		<>
-			<Section
-				title={
-					ml({
-						en: "blog post",
-						fa: "بلاگ پست",
-					}) +
-					" #" +
-					blog_id
-				}
-			>
-				<div className="px-2">
-					<Alert icon={<InfoRounded sx={{ color: "white" }} />}>
-						{ml({
-							en: `this feature will be implented soon ! track the development in github link
-							in footer`,
-							fa: "این قابلیت به زودی توسعه پیدا میکند. وضعیت پیشرفت را  میتوانید در مخزن گیت هاب این پروژه دنبال کنید - لینک در پایین صفحه قرار دارد",
-						})}
-					</Alert>
-					{/* todo */}
-				</div>
-			</Section>
-		</>
+		<Loading is_loading={blog === null} />
+			{blog !== null && (
+			<div className="m-2 border border-stone-400 p-2">
+				<img src={gen_link_to_file(`./blog_images/${blog.image_file_name}`)} className='h-48' style={{objectFit : 'contain'} } />
+				<h1 className="text-lg">{blog.title}</h1>
+				<p className="text-stone-400">last modified by {blog.username} at {new Date(Number(blog.last_modification_time)).toString() }</p>
+				<p className="mt-2">
+					{ blog.text}
+				</p>
+			</div>	
+		) }
+		
+			</>
 	);
 }
