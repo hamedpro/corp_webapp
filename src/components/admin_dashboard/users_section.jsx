56fd1d9d77c87267e83f9e4a5905e31f1b13b2e2
hamedpro <hamedyaghootpour@gmail.com>
2022-09-20 02:00:00 +0430

    new fields are added for each user in users section of admin dashboard

--- src/components/admin_dashboard/users_section.jsx
+++ src/components/admin_dashboard/users_section.jsx
@@ -1,9 +1,12 @@
+import { InfoRounded } from "@mui/icons-material";
 import { useState, useEffect } from "react";
 import { customAjax } from "../../../src/custom_ajax.js";
 import { multi_lang_helper as ml } from "../../common";
 import { CustomTable } from "../custom_table/comp.jsx";
+import { Alert } from "../alert/comp"
+import {Loading} from "../loading/comp"
 export default function UsersSection() {
-	const [users, set_users] = useState([]);
+	const [users, set_users] = useState(null);
 	function fetch_data() {
 		customAjax({
 			params: {
@@ -92,7 +95,9 @@ export default function UsersSection() {
 				en: "users:",
 				fa: "کاربران:",
 			})}
-			<CustomTable
+			<Loading is_loading={users === null} />
+			{users !== null && users.length !== 0 && (
+				<CustomTable
 				headerItems={[
 					ml({
 						en: "user_id",
@@ -106,10 +111,17 @@ export default function UsersSection() {
 						en: "is_admin",
 						fa: "دسترسی مدیر",
 					}),
+						'subscribed to email',
+						'subscribed to sms',
+						'email',
+						'phone number',
+						'time',
+						'profile_picture_file_name',
 					ml({
 						en: "options",
 						fa: "گزینه ها ",
 					}),
+					
 				]}
 				rows={users.map((user, index) => {
 					return [
@@ -145,6 +157,44 @@ export default function UsersSection() {
 							},
 						},
 						{
+							value: user.is_subscribed_to_email,
+							onClick: () => {
+								alert('functionality of changing this field will be added soon!')
+							}
+						},
+						
+						{
+							value: user.is_subscribed_to_sms,
+							onClick: () => {
+								alert('functionality of changing this field will be added soon!')
+							}
+						},
+						{
+							value: user.email,
+							onClick: () => {
+								alert('functionality of changing this field will be added soon!')
+							}
+						},
+						{
+							value: user.phone_number,
+							onClick: () => {
+								alert('functionality of changing this field will be added soon!')
+							}
+						},
+						{
+							value: user.time,
+							onClick: () => {
+								alert('functionality of changing this field will be added soon!')
+							}
+						},
+						{
+							value: user.profile_image_file_name,
+							onClick: () => {
+								alert('functionality of changing this field will be added soon!')
+							}
+						},
+
+						{
 							value: ml({
 								en: "delete his/her profile picture",
 								fa: "حذف عکس این کاربر",
@@ -161,6 +211,12 @@ export default function UsersSection() {
 					];
 				})}
 			/>
+			)}
+			{users !== null && users.length === 0 && (
+				<Alert icon={<InfoRounded />}>
+					there is not any user registered!
+				</Alert>
+			)}
 		</div>
 	);
 }
