e55222c50bbe7e6a833f4fdfc908dee77ac9e01b
hamed yaghoutpour <hamedpro30@gmail.com>
2023-08-09 03:27:38 +0330

    a little vertical overflow issue

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,11 +1,6 @@
 export function Section({ className, top_line_style, secondary, title, children, innerClassName }) {
 	return (
-		<div
-			className={
-				" overflow-y-hidden section" +
-				(typeof className == "undefined" ? "" : " " + className)
-			}
-		>
+		<div className={"section" + (typeof className == "undefined" ? "" : " " + className)}>
 			<div className={["border border-blue-400 rounded pb-2"].join(" ")}>
 				<div
 					className={["w-full h-8 rounded-t relative bg-blue-400 mb-4"].join(" ")}
