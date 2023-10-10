74343641a372f449108b4e494efdaa508ff0e123
hamed yaghoutpour <hamedpro30@gmail.com>
2023-10-10 09:38:12 +0330

    made basic responsive func of Content Slider work

--- src/components/ContentSlider.jsx
+++ src/components/ContentSlider.jsx
@@ -3,25 +3,56 @@ import { useContext } from "react";
 import { CustomImageSlider } from "./CustomImageSlider";
 import { context } from "freeflow-react";
 import { calc_file_url } from "freeflow-core/dist/utils";
+import { useWindowSize } from "@uidotdev/usehooks";
 export const ContentSlider = () => {
 	var { cache, profiles_seed, rest_endpoint } = useContext(context);
 	var content_slider_content = cache.find((ci) => ci.thing.type === "content_slider_content");
-	var file_ids = [];
-	if (content_slider_content?.thing.value !== undefined) {
-		Object.keys(content_slider_content?.thing.value).forEach((breakpoint) => {
-			file_ids = file_ids.concat(content_slider_content?.thing.value[breakpoint]);
-		});
-	}
+
 	return (
 		<div
 			className="w-full relative"
 			style={{ width: "100vw", height: "55vh" }}
 		>
-			<CustomImageSlider
-				images_sources={file_ids
-					.reverse()
-					.map((image_id) => calc_file_url(profiles_seed, rest_endpoint, image_id))}
-			/>
+			<div className="sm:hidden">
+				<CustomImageSlider
+					images_sources={(content_slider_content?.thing.value["below_sm"] || [])
+
+						.reverse()
+						.map((image_id) => calc_file_url(profiles_seed, rest_endpoint, image_id))}
+				/>
+			</div>
+			<div className="hidden sm:block md:hidden">
+				<CustomImageSlider
+					images_sources={(content_slider_content?.thing.value["sm"] || [])
+
+						.reverse()
+						.map((image_id) => calc_file_url(profiles_seed, rest_endpoint, image_id))}
+				/>
+			</div>
+			<div className="hidden md:block lg:hidden">
+				<CustomImageSlider
+					images_sources={(content_slider_content?.thing.value["md"] || [])
+
+						.reverse()
+						.map((image_id) => calc_file_url(profiles_seed, rest_endpoint, image_id))}
+				/>
+			</div>
+			<div className="hidden lg:block xl:hidden">
+				<CustomImageSlider
+					images_sources={(content_slider_content?.thing.value["lg"] || [])
+
+						.reverse()
+						.map((image_id) => calc_file_url(profiles_seed, rest_endpoint, image_id))}
+				/>
+			</div>
+			<div className="hidden xl:block 2xl:hidden">
+				<CustomImageSlider
+					images_sources={(content_slider_content?.thing.value["xl"] || [])
+
+						.reverse()
+						.map((image_id) => calc_file_url(profiles_seed, rest_endpoint, image_id))}
+				/>
+			</div>
 		</div>
 	);
 };
