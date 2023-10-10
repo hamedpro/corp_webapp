368efb6e5bff5a70e0cf927f3b4a20ed0b0884b0
hamed yaghoutpour <hamedpro30@gmail.com>
2023-10-10 09:23:26 +0330

    made ManageContentSlider working

--- src/components/ContentSlider.jsx
+++ src/components/ContentSlider.jsx
@@ -6,14 +6,19 @@ import { calc_file_url } from "freeflow-core/dist/utils";
 export const ContentSlider = () => {
 	var { cache, profiles_seed, rest_endpoint } = useContext(context);
 	var content_slider_content = cache.find((ci) => ci.thing.type === "content_slider_content");
-
+	var file_ids = [];
+	if (content_slider_content?.thing.value !== undefined) {
+		Object.keys(content_slider_content?.thing.value).forEach((breakpoint) => {
+			file_ids = file_ids.concat(content_slider_content?.thing.value[breakpoint]);
+		});
+	}
 	return (
 		<div
 			className="w-full relative"
 			style={{ width: "100vw", height: "55vh" }}
 		>
 			<CustomImageSlider
-				images_sources={(content_slider_content?.thing.value || [])
+				images_sources={file_ids
 					.reverse()
 					.map((image_id) => calc_file_url(profiles_seed, rest_endpoint, image_id))}
 			/>
--- src/components/ManageContentSlider.jsx
+++ src/components/ManageContentSlider.jsx
@@ -1,10 +1,10 @@
-import React, { useContext, useEffect, useState } from "react";
-import Select from "react-select";
+import { Fragment, useContext, useEffect, useState } from "react";
 import { ProgressBarModal } from "./ProgressBarModal";
 import { Section } from "./Section";
 import { StyledDiv } from "./StyledElements";
 import { context } from "freeflow-react";
 import { calc_file_url } from "freeflow-core/dist/utils";
+import Select from "react-select";
 export const ManageContentSlider = () => {
 	var {
 		cache,
@@ -19,6 +19,11 @@ export const ManageContentSlider = () => {
 		percent: undefined,
 	});
 	var content_slider_content = cache.find((ci) => ci.thing.type === "content_slider_content");
+	var [current_breakpoint, set_current_breakpoint] = useState({
+		value: "below_sm",
+		label: "below_sm",
+	});
+	var breakpoints = ["below_sm", "sm", "md", "lg", "xl", "2xl"];
 
 	async function upload_new_image() {
 		var file = document.getElementById("new_image_input").files[0];
@@ -45,33 +50,37 @@ export const ManageContentSlider = () => {
 			is_uploading: false,
 			percent: undefined,
 		});
-
 		alert("با موفقیت انجام شد ");
 		if (content_slider_content === undefined) {
 			await request_new_thing({
 				thing: {
 					type: "content_slider_content",
-					value: [new_file_id],
+					value: { [current_breakpoint.value]: [new_file_id] },
 				},
 				thing_privileges: { read: "*", write: [-1] },
 			});
 		} else {
 			await request_new_transaction({
 				thing_id: content_slider_content.thing_id,
-				new_thing_creator: (prev) => ({
-					...prev,
-					value: prev.value.concat([new_file_id]),
-				}),
+				new_thing_creator: (prev) => {
+					if (prev.value[current_breakpoint.value] === undefined) {
+						prev.value[current_breakpoint.value] = [];
+					}
+					prev.value[current_breakpoint.value].push(new_file_id);
+					return prev;
+				},
 			});
 		}
 	}
-	async function delete_image(file_id) {
+	async function delete_image(breakpoint, file_id) {
 		if (window.confirm("ایا اطمینان دارید میخواهید این عکس را حذف کنید ؟؟") !== true) return;
 		await request_new_transaction({
-			new_thing_creator: (prev) => ({
-				...prev,
-				value: prev.value.filter((image_file_id) => image_file_id !== file_id),
-			}),
+			new_thing_creator: (prev) => {
+				var tmp = JSON.parse(JSON.stringify(prev));
+				prev.value[breakpoint] = prev.value[breakpoint].filter((i) => i !== file_id);
+				console.log(prev);
+				return prev;
+			},
 			thing_id: content_slider_content.thing_id,
 		});
 		alert("با موفقیت انجام شد");
@@ -89,25 +98,43 @@ export const ManageContentSlider = () => {
 				<Section
 					title="عکس های فعلی"
 					className="mt-2"
-					innerClassName="p-2 flex space-x-2"
+					innerClassName="p-2  space-x-2"
 				>
-					{(content_slider_content?.thing.value || []).map((image_file_id) => {
+					{breakpoints.map((breakpoint) => {
+						var file_ids = content_slider_content?.thing.value[breakpoint] || [];
 						return (
-							<div
-								key={image_file_id}
-								className="border border-blue-400 rounded w-40 aspect-auto mx-1"
-							>
-								<h1 className="text-center">#{image_file_id}</h1>
-								<img
-									src={calc_file_url(profiles_seed, rest_endpoint, image_file_id)}
-								/>
-								<button
-									onClick={() => delete_image(image_file_id)}
-									className="text-center w-full h-6 bg-red-500 text-white duration-300 hover:bg-red-600 rounded-b"
-								>
-									حذف کردن این عکس
-								</button>
-							</div>
+							<Fragment key={breakpoint}>
+								{breakpoint}
+								<hr />
+								{file_ids.length === 0 && "این سایز هیچ عکسی ندارد"}
+								{file_ids.map((image_file_id) => {
+									return (
+										<div
+											key={breakpoint + image_file_id}
+											className="border border-blue-400 rounded w-40 aspect-auto mx-1"
+										>
+											<h1 className="text-center">#{image_file_id}</h1>
+											<img
+												src={calc_file_url(
+													profiles_seed,
+													rest_endpoint,
+													image_file_id
+												)}
+											/>
+											<button
+												onClick={() =>
+													delete_image(breakpoint, image_file_id)
+												}
+												className="text-center w-full h-6 bg-red-500 text-white duration-300 hover:bg-red-600 rounded-b"
+											>
+												حذف کردن این عکس
+											</button>
+										</div>
+									);
+								})}
+								<br />
+								<br />
+							</Fragment>
 						);
 					})}
 				</Section>
@@ -121,6 +148,17 @@ export const ManageContentSlider = () => {
 						type="file"
 					/>
 					<br />
+					<br />
+					<p>سایز این عکس:</p>
+					<Select
+						className="text-black"
+						options={breakpoints.map((breakpoint) => ({
+							value: breakpoint,
+							label: breakpoint,
+						}))}
+						value={current_breakpoint}
+						onChange={(newValue) => set_current_breakpoint(newValue)}
+					/>
 					<StyledDiv
 						onClick={upload_new_image}
 						className="mt-2"
